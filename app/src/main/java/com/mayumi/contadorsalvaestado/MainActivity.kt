13ad061b7658de7d170bf9b5b027a2da185b8283
cymayumi <50723813+cymayumi@users.noreply.github.com>
2019-06-18 18:12:33 -0300

    Initial commit

---
+++ app/src/main/java/com/mayumi/contadorsalvaestado/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.mayumi.contadorsalvaestado
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var conta: Int = 0
+    val CONTADOR = "contador" //criamos uma variável CONTADOR do tipo chave-valor
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        //método irá salvar o valor já contado, ao recriar a activity (onCreate) o valor salvo será usado
+
+        outState?.run {
+            //tudo o que desejarmos salvar deve ser declarado aqui (chave/valor)
+
+            putInt(CONTADOR, conta)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // vai avaliar de estamos recriando ou nao uma nova instancia
+        if (savedInstanceState != null) {
+            with(savedInstanceState) {
+
+                conta = getInt(CONTADOR)
+                tv_contador.setText(conta.toString())
+            }
+        } else {
+            conta = 0
+        }
+
+
+        btn_add.setOnClickListener {
+
+            conta = conta + 1
+
+            tv_contador.setText(conta.toString())
+
+        }
+    }
+
+}
